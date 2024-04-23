305c628f4a693b5890744ca64ef384af83833c1c
Chijindu Nwokeohuru <chijindu.nwokeohuru@gmail.com>
2024-04-24 00:08:32 +0100

    commit update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -5,14 +5,20 @@ const request = require('request');
 const movieId = process.argv[2];
 const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
-request(apiUrl, function (error, response, body) {
+request({
+  url: apiUrl,
+  rejectUnauthorized: false
+}, function (error, response, body) {
   if (!error && response.statusCode === 200) {
     const movieData = JSON.parse(body);
 
     console.log(`Characters of "${movieData.title}":`);
 
     movieData.characters.forEach((characterUrl) => {
-      request(characterUrl, function (charError, charResponse, charBody) {
+      request({
+        url: characterUrl,
+        rejectUnauthorized: false // Bypass SSL certificate verification
+      }, function (charError, charResponse, charBody) {
         if (!charError && charResponse.statusCode === 200) {
           const characterData = JSON.parse(charBody);
 
